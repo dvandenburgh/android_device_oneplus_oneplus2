a18bf4a8f8daac5e9a7d3558b9405e9339f2da1e
Dhruva Krishnamurthy <dhruvakm@gmail.com>
2016-09-03 10:50:59 -0700

    Sensors driver wrapper to use AOSP rotation vector
    OnePlus2 has an unreliable rotation vector in custom ROMs.
    We fallback on AOSP implementation for rotation vector
    
    Change-Id: I54bc69ba52f6e00ddac97e73a64c4e614499662d

--- device.mk
+++ device.mk
@@ -241,6 +241,10 @@ PRODUCT_PACKAGES += \
     sensors.ssc.wrapper
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf \
+    $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:system/etc/sensors/sensor_def_qcomdev.conf
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:system/etc/sensors/hals.conf
 
 # USB
