51274c70915f1c6886c6ee6f258f7cbe9eb6607a
dvandenburgh <vandend278@cobleskill.edu>
2018-06-27 18:06:30 -0400

    oneplus2: remove OneplusDoze
    Change-Id: Ib7b5289ccc90a263a7d3f8451626ec78b15d1f4f

--- device.mk
+++ device.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
