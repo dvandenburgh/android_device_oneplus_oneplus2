8bcd1bb7fd157888a5c44e74113759e0671fd117
dvandenburgh <vandend278@cobleskill.edu>
2018-10-15 20:41:58 -0400

    op2: drop OneplusDoze
    and set overlays
    
    Change-Id: I94909c0264c36b3f98b7ff85e2686f12799fdbba

--- device.mk
+++ device.mk
@@ -147,10 +147,6 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
